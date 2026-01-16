2b4486cc9ec16fa5fa65dded025596ae446092a7
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-16 18:29:32 +0000

    Create __init__.py

---
+++ a7do/__init__.py
@@ -0,0 +1,10 @@
+a7do/__init__.py
+a7do/background_core/__init__.py
+a7do/core/__init__.py
+a7do/embodiment/__init__.py
+a7do/cognition/__init__.py
+a7do/policies/__init__.py
+
+world/__init__.py
+sled/__init__.py
+shared/__init__.py
